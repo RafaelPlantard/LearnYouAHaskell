519ac359246a352b7f3a83de7f71ab0d1e4e6526
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-12-10 20:08:11 -0200

    Add type declaration to some functions

--- baby.hs
+++ baby.hs
@@ -14,6 +14,7 @@ boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [x | x <- st, x `elem` ['A'..'Z']]
 
 triangles = [(a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10]]
@@ -21,3 +22,15 @@ triangles = [(a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10]]
 rightTriangles = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a ^ 2 + b ^ 2 == c ^ 2]
 
 rightTriangles' = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a ^ 2 + b ^ 2 == c ^ 2, a + b + c == 24]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
