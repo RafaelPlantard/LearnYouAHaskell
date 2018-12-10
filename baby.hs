90789740c18fb4c2a264ff7e0fd72751cba5ee79
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-12-09 23:54:29 -0200

    List comprehension and Tuples

--- baby.hs
+++ baby.hs
@@ -11,3 +11,13 @@ doubleSmallNumber' x = doubleSmallNumber x + 1
 conanO'Brien = "It's a-me, Conna O'Brien!"
 
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [x | x <- st, x `elem` ['A'..'Z']]
+
+triangles = [(a, b, c) | c <- [1..10], b <- [1..10], a <- [1..10]]
+
+rightTriangles = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a ^ 2 + b ^ 2 == c ^ 2]
+
+rightTriangles' = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a ^ 2 + b ^ 2 == c ^ 2, a + b + c == 24]
